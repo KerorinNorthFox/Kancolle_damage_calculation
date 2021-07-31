15990786259f2608cdd89f87b63a408f54a75a5a
m20_KerorinNF <66837061+KerorinNorthFox@users.noreply.github.com>
2021-07-31 23:17:26 +0900

    Update damage_module.cpp

--- c/damage_module.cpp
+++ c/damage_module.cpp
@@ -13,7 +13,31 @@ int engagement_form_func(){
 }
 
 float correction_before_cap_func(int engagement_form, int attack_formation, int damaged_condition, int attack_mode);{
-	
+        float engagement_form_correction;
+        if (engagement_form == 1){
+            engagement_for_correction = 1.0;
+        }else if (engagement_form == 2){
+            engagement_form_correction = 0.8;
+        }else if (engagement_form == 3){
+            engagement_form_correction = 1.2;
+        }else if (engagement_form == 4){
+            engagement_form_correction = 0.6;
+        }
+
+        float attack_formation_firepower, attack_formation_torpedo;
+        if (attack_formation == 1){
+            attack_formation_firepower = 1.0;
+            attack_formation_torpedo = 1.0;
+        }else if (attack_formation == 2){
+            attack_formation_firepower = 1;
+            attack_formation_torpedo = 1;
+        }else if (attack_formation == 3){
+            attack_formation_correction = 1;
+        }else if (attack_formation == 4){
+            attack_formation_correction = 1;
+        }else if (attack_formation == 5){
+            attack_formation_correction = 1;
+        }
 }
 /*
 float
