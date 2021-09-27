8cef5eddef9212515fe564a05026377bca7883a2
m20_KerorinNF <66837061+KerorinNorthFox@users.noreply.github.com>
2021-09-27 11:11:33 +0900

    Update damage_calc.cpp

--- c/damage_calc.cpp
+++ c/damage_calc.cpp
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	int attack_formation, damaged_condition, attack_mode, firepower, enemy_armor, now_hp;
+	int attack_formation, damaged_condition, attack_mode, firepower, enemy_armor, now_hp, loopnumber;
 	printf(">>陣形選択/n#1:単縦陣\n#2:複縦陣\n#3:輪形陣\n#4:梯形陣\n#5:単横陣\n:");
 	scanf("%d", &attack_formation);
 	printf("\n>>艦状態\n#1:小破以下\n#2:中破\n#3:大破\n:");
@@ -16,21 +16,37 @@ int main(void)
 	scanf("%d", &enemy_armor);
 	printf("\n>>現在の敵HP:");
 	scanf("%d", &now_hp);
+	printf("\n>>繰り返す回数:");
+	scanf("%d", &loopnumber);
 	
-	int engagement_form;
-	engagement_form = engagement_form_func();
-	printf("%d", engagement_form);
+	float correction_before_cap[];
+	float basic_attackpower[];
+	float attackpower_before_cap[];
+	float attackpower_after_cap[];
+	float final_attackpower[];
+	float vitality[];
+	float results_list[];
+	int prob_hit, prob_throught_the_armor, prob_percentage_damage, prob_miss, miss_counter = 0;
 	
-	float correction_before_cap;
-	correction_before_cap = correction_before_cap_func(engagement_form, attack_formation, damaged_condition, attack_mode);
-	printf("\n>>キャップ前補正:%f", correction_before_cap);
+	printf("\n######################################################");
 	
-	float basic_attackpower;
-	basic_attackpower = basic_attackpower(firepower);
-	printf("\n>>基本攻撃力:%f", basic_attackpower);
-	
-	float attackpower_before_cap;
-	attackpower_before_cap = attackpower_before_cap_func(basic_attackpower, correction_before_cap);
-	printf("\n>>キャップ前攻撃力:%f", attackpower_before_cap);
-	
+	for(i=0; i<loopnumber; i++){
+		int engagement_form;
+		engagement_form = engagement_form_func();
+		#printf("%d", engagement_form);
+
+		correction_before_cap[i] = correction_before_cap_func(engagement_form, attack_formation, damaged_condition, attack_mode);
+		#printf("\n>>キャップ前補正:%f", correction_before_cap[i]);
+
+		basic_attackpower[i] = basic_attackpower(firepower);
+		#printf("\n>>基本攻撃力:%f", basic_attackpower[i]);
+
+		attackpower_before_cap[i] = attackpower_before_cap_func(basic_attackpower[i], correction_before_cap[i]);
+		#printf("\n>>キャップ前攻撃力:%f", attackpower_before_cap[i]);
+		
+		attackpower_after_cap[i] = attackpower_after_cap_func(attackpower_before_cap[i]);
+		#printf("\n>>キャップ後攻撃力:%f", attackpower_after_cap[i]);
+		
+		final_attackpower[i] = final_attackpower_func(attackpower_after_cap[i]);
+		#printf("\n>>最終攻撃力:%f", final_attackpower[i]);
 }
